00fb1bc7c4117a2b160359fe72bbeb2d194d630d
karthick <kartick619@gmail.com>
2023-12-21 10:36:32 +0530

    Authentic services

--- Ticketing/auth/src/models/user.ts
+++ Ticketing/auth/src/models/user.ts
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose';
-import { Password } from '../services/password';
 
 // An interface that describes the properties
 // that are requried to create a new User
@@ -31,15 +30,6 @@ const userSchema = new mongoose.Schema({
     required: true
   }
 });
-
-userSchema.pre('save', async function(done) {
-  if (this.isModified('password')) {
-    const hashed = await Password.toHash(this.get('password'));
-    this.set('password', hashed);
-  }
-  done();
-});
-
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
